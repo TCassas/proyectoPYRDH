d45e685d546f077426bf5fceebc42775792399d9
TCassas <tomascassas@gmail.com>
2019-11-26 20:58:44 -0300

    Ramiro Commit pagina Login

---
+++ pag/pagina.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="estilos.css">
+  <title>Preguntados</title>
+  <h1>Registrate</h1>
+</head>
+
+<body>
+    <section></section>
+    <section>
+      <form action="resultado.html" method="POST">
+      <div class="contenedor">
+        <label for="nombre">Usuario</label> </br>
+        </br> <input type="text" name="name" id="nombre">
+      </div>
+
+    </br> <div class="contenedor">
+        <label for="password">Contraseña</label> </br>
+        </br> <input type="password" name="password" id="password">
+      </div>
+
+      </br> <div class="contenedor">
+        <label for="pass2">Verificar Contraseña</label> </br>
+      </br> <input type="password" name="" id="pass2">
+    </div>
+
+    </br> <div class="contenedor">
+        <label for="email">Correo</label> </br>
+      </br> <input type="email" name="email" id="email">
+      </div </br>
+
+    </br> <div class="contenedor">
+        <label for="dias">Fecha de Nacimiento</label> </br>
+      </br>  <input type="date">
+    </div>
+
+  </br> <div class="contenedor">
+  </br> <input type="submit" value="Enviar">
+  </div>
+  </section>
+
+  </form></br>
+</body>
+</html>
