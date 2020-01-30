0d8c2da1cb44f9f0f95216faeaa9e4bb260ae620
Tomás Cassas <tomascassas@gmail.com>
2020-01-30 04:28:43 -0300

    xd

--- borrar.php
+++ borrar.php
@@ -5,10 +5,11 @@
 
   $id = $_GET["id"];
 
-  $query = $db->prepare("DELETE FROM cuestionarios
-                         INNER JOIN preguntas4respuestas ON cuestionarios.id = preguntas4respuestas.cuestionario_id
-                         INNER JOIN preguntasvof ON cuestionarios.id = preguntasvof.cuestionario_id
-                         WHERE id = $id");
+  //al menos lo intenté
+  // $query = $db->prepare("DELETE FROM cuestionarios
+  //                        INNER JOIN preguntas4respuestas ON cuestionarios.id = preguntas4respuestas.cuestionario_id
+  //                        INNER JOIN preguntasvof ON cuestionarios.id = preguntasvof.cuestionario_id
+  //                        WHERE id = $id");
   $query->execute();
 
   header("Location: misCuestionarios.php");
